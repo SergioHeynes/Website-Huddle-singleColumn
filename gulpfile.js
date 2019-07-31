f8f1df8346518d0f5f420573ef1ab2a7ad1dd1d9
Sergio Heynes <sergio.heynes@gmail.com>
2019-07-31 15:22:59 -0500

    Build docs folder to deploy on GitHub pages. Add readme.md and style-guide files.

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,28 @@ const sass = require('gulp-sass'),
 postcss = require('gulp-postcss'), 
 autoprefixer = require('autoprefixer');
 
+// For deploy
+const imagemin = require('gulp-imagemin'), // optimize images
+imageminPngquant = require('imagemin-pngquant'),
+imageminJpegRecompress = require('imagemin-jpeg-recompress'),
+del = require('del'), // delete
+usemin = require('gulp-usemin'), 
+rev = require('gulp-rev'), // rev our files
+cssnano = require('gulp-cssnano'); // compress CSS
+
+
+function previewDist() {
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: 'docs'
+        }
+    });
+}
+
+exports.previewDist = previewDist;
+
+
 
 function styles() {
     return gulp.src('./app/assets/styles/main.scss')
@@ -32,3 +54,33 @@ function watch() {
 }
 
 exports.watch = watch;
+
+
+function images() {
+    return gulp.src('./app/assets/images/**/*.{png,jpeg,jpg,svg,gif}')
+        .pipe(imagemin([
+            imagemin.gifsicle(),
+            imagemin.jpegtran(),
+            imagemin.optipng(),
+            imagemin.svgo(),
+            imageminPngquant(),
+            imageminJpegRecompress()
+        ]))
+        .pipe(gulp.dest('./docs/assets/images'));
+}
+
+exports.images = images;
+
+function clean() {
+    return del(['./docs']);
+}
+
+function useminTask() {
+    return gulp.src('./app/index.html')
+        .pipe(usemin({
+            css: [function() {return rev()}, function() {return cssnano()}]
+        }))
+        .pipe(gulp.dest('./docs'));
+}
+
+exports.build = gulp.series(clean, images, styles, useminTask);
